f62df5eb1c7621f2bc1abe15acc510fcaee73d5a
hasan-tfaily <hasantfaily@hasans-MacBook-Air.local>
2025-05-06 12:53:18 +0300

    changes

--- src/app/_components/ShareButtons.jsx
+++ src/app/_components/ShareButtons.jsx
@@ -1,31 +1,54 @@
 "use client";
 
-import {FacebookShareButton, TwitterShareButton, LinkedinShareButton } from 'next-share';
-
 const ShareButtons = () => {
+  const socialLinks = [
+    {
+      link: "https://www.facebook.com/qomra.studios",
+      icon: "fab fa-facebook-f",
+      title: "Facebook",
+    },
+    {
+      link: "https://www.instagram.com/qomra.studios/",
+      icon: "fab fa-instagram",
+      title: "Instagram",
+    },
+    {
+      link: "https://www.tiktok.com/@qomra.studios",
+      icon: "fab fa-tiktok",
+      title: "TikTok",
+    },
+    {
+      link: "https://www.youtube.com/@QomraStudios",
+      icon: "fab fa-youtube",
+      title: "YouTube",
+    },
+    {
+      link: "https://www.linkedin.com/company/qomra-studios/",
+      icon: "fab fa-linkedin-in",
+      title: "LinkedIn",
+    },
+  ];
+
   return (
-    <>
-        <div className="mil-share mil-mb120 mil-up">
-            <p>Share:</p>
-            <ul className="mil-social">
-                <li className="mil-c-gone">
-                    <FacebookShareButton>
-                        <i className="fab fa-facebook-f"></i>
-                    </FacebookShareButton>
-                </li>
-                <li className="mil-c-gone">
-                    <TwitterShareButton>
-                        <i className="fab fa-twitter"></i>
-                    </TwitterShareButton>
-                </li>
-                <li className="mil-c-gone">
-                    <LinkedinShareButton>
-                        <i className="fab fa-linkedin"></i>
-                    </LinkedinShareButton>
-                </li>
-            </ul>
-        </div>
-    </>
+    <div className="mil-share mil-mb120 mil-up">
+      <p>Follow Us:</p>
+      <ul className="mil-social">
+        {socialLinks.map((social, index) => (
+          <li key={index} className="mil-c-gone">
+            <a
+              href={social.link}
+              target="_blank"
+              rel="noreferrer noopener"
+              title={social.title}
+              aria-label={social.title}
+            >
+              <i className={social.icon}></i>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
+
 export default ShareButtons;
